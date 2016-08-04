facaa44d38dda00b55741e735ebf369496296d23
Dezso Ribli <ribli@Dezsos-MacBook-Air.local>
2016-08-04 14:01:47 +0200

    upadted the config/param readig files

--- src/sps_options.cpp
+++ src/sps_options.cpp
@@ -1,12 +1,17 @@
 #include "sps_options.h"
 
 /*
- initially set platfrom and device and measurement to no choice
+ initially set platfrom and device and measurement to  0
+ set measurement file to a default one
  */
 sps_options::sps_options(){
-    this->device= -1;
-    this->platform= -1;
-    this->sdss_measurement_fname="none";
+    this->device= 0;
+    this->platform= 0;
+    
+    std::string path=getenv("SPSFAST_PATH");
+    std::stringstream sstr;
+    sstr<<path<<"/input/testspec1.csv";
+    sstr>>this->sdss_measurement_fname;
 }
 
 /*
@@ -16,60 +21,63 @@ int sps_options::read_config(std::string input_filename)
 {
     //open file
     std::ifstream infile(input_filename.c_str(), std::ifstream::in );
-    //check file
-    if(!(infile))
-    {
-        std::cout<<"\nERROR CAN'T OPEN CONFIG FILE: "<<input_filename<<"\n"<<std::endl;
-        return 1;
+    if(!infile.is_open()){
+        std::cerr<<"\nERROR CAN'T OPEN CONFIG FILE: "<<input_filename<<"\n"<<std::endl;
+        exit(1);
     }
     
-    std::string str;
-    while(getline(infile,str))
-    {
+    //loop over lines and get params
+    std::string line,temp_str;
+    std::map<std::string,std::string> params;
+    while(getline(infile,line)){
         std::stringstream sstr;
-        std::vector<std::string> tempvec;
-        
-        sstr<<str;
-        while(sstr>>str)
-        {
-            tempvec.push_back(str);
+        std::vector<std::string> temp_vec;
+        //read whole line into vector
+        sstr<<line;
+        while(sstr>>temp_str){
+            temp_vec.push_back(temp_str);
         }
-        
-        if (tempvec.size() !=0 )
-        {
-            //reading parameters
-            if( tempvec[0]=="platform"){
-                this->platform= (int) strtol(tempvec[1].c_str(),NULL,10);
-            }
-            else if( tempvec[0]=="device"){
-                this->device= (int) strtol(tempvec[1].c_str(),NULL,10);
-            }
-            else if( tempvec[0]=="measurement"){
-                this->sdss_measurement_fname = tempvec[1];
-            }
+        //if its a key pair save it
+        if (temp_vec.size() == 2){
+            params.insert(std::pair<std::string,std::string>(temp_vec[0],temp_vec[1]));
         }
-        
     }
-    infile.close();
     
+    //save the ones i need, fall back to deafult value if not set
+    std::map<std::string,std::string>::iterator it;
+    it= params.find("platform");
+    if(it != params.end()){
+        this->platform=(int) strtol( params["platform"].c_str(),NULL,10);
+    }
+    it= params.find("device");
+    if(it != params.end()){
+        this->device=(int) strtol( params["device"].c_str(),NULL,10);
+    }
+    it= params.find("measurement");
+    if(it != params.end()){
+        this->sdss_measurement_fname = params["measurement"];
+
+    }
+
+    infile.close();
     return 0;
 }
 
 
 
 
-//read parameter file
+/*
+ read parameter file into a vector of maps
+*/
 std::vector<std::map<std::string,double> > sps_options::read_param_file(std::string infilename){
     //open file
     std::ifstream infile(infilename.c_str());
-    //check file
-    if(! infile.is_open() )
-    {
+    if(! infile.is_open() ){
         std::cerr<<"\nERROR CAN'T OPEN PARAMETER FILE: "<<infilename<<"\n"<<std::endl;
         exit(1);
     }
     
-    //read param names
+    //read param names for the first line
     std::string line,param_name;
     std::stringstream temp_sstr;
     std::vector<std::string> param_names;
@@ -79,7 +87,7 @@ std::vector<std::map<std::string,double> > sps_options::read_param_file(std::str
         param_names.push_back(param_name);
     }
     
-    //reading params
+    //read params from the other lines
     std::vector<std::map<std::string,double> > params;
     while ( getline (infile,line) ){
         std::map<std::string,double> temp_params;
@@ -97,7 +105,6 @@ std::vector<std::map<std::string,double> > sps_options::read_param_file(std::str
     }
     
     infile.close();
-    
     return params;
     
 }
